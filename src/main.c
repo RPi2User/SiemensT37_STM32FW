645a6cccca49661bee7f7dc6d99613b257176925
RPi2User <florian@hatzfeld.biz>
2025-06-05 11:05:12 +0200

    Code Structure in main.c

---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* This project is the main source for the STM32 Firmware
+ * This source has 3 Main objectives
+ *  1. PRINT STUFF VIA GPIO IN CCITT-2 STD
+ *  2. Get Data from USB (via 9600N1 Serial)
+ *  3. Get Data from WiFi (via ESP8266)
+ *
+ */
+
+int main()
+{
+	while(1){
+		// do smth important
+        //
+	}
+	return 0;
+}
