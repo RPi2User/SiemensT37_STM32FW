e29b92eae0d8cdef125a0b8e46251fead7936fac
RPi2User <gm.aier1905@gmail.com>
2025-07-04 00:11:47 +0200

    First code in booTY.c

--- Core/Src/booTY.c
+++ Core/Src/booTY.c
@@ -2,17 +2,65 @@
 #include <stdlib.h>
 #include "tty.h"
 
+// -----------------------------------------------------------------
 
-// Writes Preamble to TTY
-int* init(int* currentBuffer){
+const int VALID_MODES[] = {
+	symbol.b, symbol.w, symbol.n, symbol.period, symbol.figs
+}:
 
+const int VALID_VALS[] = {
+	symbol.n0, symbol.n1, symbol.n2, symbol.n3, symbol.n4, 
+	symbol.n5, symbol.n6, symbol.n7, symbol.n8, symbol.n9
+};
+
+const int VALID_LINEBREAKS[] = {
+	symbol.c, symbol.r, symbol.l, symbol.f, symbol.n
+}; // `NL` for EBCDIC compatibilty
+
+// -----------------------------------------------------------------
+
+/* Writes Preamble to TTY
+ *    - 5x ltrs (delay for TTY Hardware to settle)
+ *    - 1x figs (for first "real" char)
+ *    - 1x `?` to encourage user input
+ *    - 1x ltrs to let the user in ltrs-mode
+ *    - 1x space for visual appeal
+ */
+int* booTYinit(int* currentBuffer){
 	for (int i = 0; i <= 5; i++)
 		currentBuffer = appendSymbol(currentBuffer, symbol.ltrs);
 	currentBuffer = appendSymbol(currentBuffer, symbol.figs);
 	currentBuffer = appendSymbol(currentBuffer, symbol.question);
+	currentBuffer = appendSymbol(currentBuffer, symbol.ltrs);
+	currentBuffer = appendSymbol(currentBuffer, symbol.space);
 
 	return currentBuffer;
 }
 
-void 
+/* Echoes User Input
+ * This function waits for a valid user input terminated by a '.'
+ * and removes garbage.
+ *    - when in ltrs all additional ltrs symbols will be ignored
+ *    - when in figs all additional figs symbols will be ignored
+ *    - all illegal chars won't be echoed
+ *    - duplicate ltrs (a-z) will be ignored
+
+
+ */
+void booTYshell(int* currentBuffer){
+	
+}
+
+int* readLine(int line_terminator){
+
+}
+
+int readKey(){
+}
+
+
+// prints stuff to TTY
+void fart(int symbol){
+	TTY_WRITE(symbol);
+}
 
